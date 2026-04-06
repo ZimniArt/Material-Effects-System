414501f2833866708f869d26788f8ea823fcc110
V.Zimnicki <zimni.art@gmail.com>
2026-04-06 23:15:16 +0300

    dropdown menu

--- Assets/Vzimn/Material Effects System/Editor/ShaderGUI.cs
+++ Assets/Vzimn/Material Effects System/Editor/ShaderGUI.cs
@@ -6,7 +6,7 @@ public class ShaderGUI : UnityEditor.ShaderGUI
 { 
     public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
     {
-       void DrawProperty(string property, string inspectorName)
+        void DrawProperty(string property, string inspectorName)
         {
             materialEditor.ShaderProperty(FindProperty(property, properties), inspectorName);
         }
@@ -18,9 +18,32 @@ public class ShaderGUI : UnityEditor.ShaderGUI
             v3 = EditorGUILayout.Vector3Field(inspectorName, v3);
             rawProperty.vectorValue = new Vector4(v3.x, v3.y,v3.z, v4.w);
         }
+        void v4_DropDown(string property, string inspectorName, string[] options)
+        {
+            var rawProperty = FindProperty(property, properties);
+            Vector4 v4 = rawProperty.vectorValue;
+
+            int selected = 0;
+
+            if (v4.x == 1) selected = 0;
+            else if (v4.y == 1) selected = 1;
+            else if (v4.z == 1) selected = 2;
+            else if (v4.w == 1) selected = 3;
+
+            selected = EditorGUILayout.Popup(inspectorName, selected, options);
+
+            Vector4 new_v4 = Vector4.zero;
+            if (selected == 0) new_v4.x = 1;
+            else if (selected == 1) new_v4.y = 1;
+            else if (selected == 2) new_v4.z = 1;
+            else if (selected == 3) new_v4.w = 1;
+            rawProperty.vectorValue = new_v4;
+        }
 
         //Main Texture
-        DrawProperty("_frag_uv_world_local_view", "Space");
+        string[] spaceOptions = { "uv", "world", "local", "view" };
+        v4_DropDown("_frag_uv_world_local_view", "Space", spaceOptions);
+
         DrawProperty_v3("_frag_plane_XYZ", "Plane");
         DrawProperty("_main_texture", "Texture");
         DrawProperty("_main_tint_color", "Color");
