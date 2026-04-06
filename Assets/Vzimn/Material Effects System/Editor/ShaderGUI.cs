7ee05e0102d9af5573847db677b85aa38e0251b3
V.Zimnicki <zimni.art@gmail.com>
2026-04-06 22:57:33 +0300

    vector4 to vector 3 shortening

--- Assets/Vzimn/Material Effects System/Editor/ShaderGUI.cs
+++ Assets/Vzimn/Material Effects System/Editor/ShaderGUI.cs
@@ -8,11 +8,20 @@ public class ShaderGUI : UnityEditor.ShaderGUI
     {
        void DrawProperty(string property, string inspectorName)
         {
-            materialEditor.ShaderProperty(FindProperty(property, properties), "Space");
+            materialEditor.ShaderProperty(FindProperty(property, properties), inspectorName);
         }
+        void DrawProperty_v3(string property, string inspectorName)
+        {
+            var rawProperty = FindProperty(property, properties);
+            Vector4 v4 = rawProperty.vectorValue;
+            Vector3 v3 = rawProperty.vectorValue;
+            v3 = EditorGUILayout.Vector3Field(inspectorName, v3);
+            rawProperty.vectorValue = new Vector4(v3.x, v3.y,v3.z, v4.w);
+        }
+
         //Main Texture
         DrawProperty("_frag_uv_world_local_view", "Space");
-        DrawProperty("_frag_plane_XYZ", "Plane");
+        DrawProperty_v3("_frag_plane_XYZ", "Plane");
         DrawProperty("_main_texture", "Texture");
         DrawProperty("_main_tint_color", "Color");
         DrawProperty("_Alpha_cliping_treshold", "Threshold");
