5e49aced1139dd30c6139c9e6eb6013d115d8551
V.Zimnicki <zimni.art@gmail.com>
2026-04-06 22:02:17 +0300

    Basic GUI

--- Assets/Vzimn/Material Effects System/Editor/ShaderGUI.cs
+++ Assets/Vzimn/Material Effects System/Editor/ShaderGUI.cs
@@ -2,13 +2,29 @@ using UnityEngine;
 using UnityEditor;
 
 public class ShaderGUI : UnityEditor.ShaderGUI
-{
+{ 
     public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
     {
-        // Example: draw ONE property manually
-        MaterialProperty alpha_clip = FindProperty("_Alpha_cliping_treshold", properties);
+        //Main Texture
 
-        EditorGUILayout.LabelField("Alpha clip", EditorStyles.boldLabel);
-        materialEditor.ShaderProperty(alpha_clip, "Strength");
+        MaterialProperty MainSpace = FindProperty("_frag_uv_world_local_view", properties);
+        materialEditor.ShaderProperty(MainSpace, "Space");
+
+        MaterialProperty MainPlane = FindProperty("_frag_plane_XYZ", properties);
+        materialEditor.ShaderProperty(MainPlane, "Plane");
+
+        MaterialProperty MainTexture = FindProperty("_main_texture", properties);
+        materialEditor.ShaderProperty(MainTexture, "Defuse");
+
+        MaterialProperty color = FindProperty("_main_tint_color", properties);
+        materialEditor.ShaderProperty(color, "Color");
+
+        MaterialProperty AlphaClippingThreshold = FindProperty("_Alpha_cliping_treshold", properties);
+        materialEditor.ShaderProperty(AlphaClippingThreshold, "Alpha Clipping Threshold");
+
+
+
     }
 }
+
+
