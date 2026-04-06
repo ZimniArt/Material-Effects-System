582b108da58afe195597532f5fdaaab258f9c0f1
V.Zimnicki <zimni.art@gmail.com>
2026-04-06 22:22:01 +0300

    helper function for property drawing

--- Assets/Vzimn/Material Effects System/Editor/ShaderGUI.cs
+++ Assets/Vzimn/Material Effects System/Editor/ShaderGUI.cs
@@ -1,30 +1,26 @@
 using UnityEngine;
 using UnityEditor;
+using System.Web;
 
 public class ShaderGUI : UnityEditor.ShaderGUI
 { 
     public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
     {
+       void DrawProperty(string property, string inspectorName)
+        {
+            materialEditor.ShaderProperty(FindProperty(property, properties), "Space");
+        }
         //Main Texture
-
-        MaterialProperty MainSpace = FindProperty("_frag_uv_world_local_view", properties);
-        materialEditor.ShaderProperty(MainSpace, "Space");
-
-        MaterialProperty MainPlane = FindProperty("_frag_plane_XYZ", properties);
-        materialEditor.ShaderProperty(MainPlane, "Plane");
-
-        MaterialProperty MainTexture = FindProperty("_main_texture", properties);
-        materialEditor.ShaderProperty(MainTexture, "Defuse");
-
-        MaterialProperty color = FindProperty("_main_tint_color", properties);
-        materialEditor.ShaderProperty(color, "Color");
-
-        MaterialProperty AlphaClippingThreshold = FindProperty("_Alpha_cliping_treshold", properties);
-        materialEditor.ShaderProperty(AlphaClippingThreshold, "Alpha Clipping Threshold");
-
-
-
+        DrawProperty("_frag_uv_world_local_view", "Space");
+        DrawProperty("_frag_plane_XYZ", "Plane");
+        DrawProperty("_main_texture", "Texture");
+        DrawProperty("_main_tint_color", "Color");
+        DrawProperty("_Alpha_cliping_treshold", "Threshold");
+        
+        //Texture Distrorion
     }
+
+
 }
 
 
